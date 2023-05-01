b0b13aa860f68c62c6e3aac7383a84c21fd14477
digant30 <digantgandhi30@gmail.com>
2023-05-01 12:04:45 +0530

    Added

---
+++ test.js
@@ -0,0 +1,10 @@
+var exec = require('child_process').exec;
+
+exec('./deployChaincode.sh', function (error, stdOut, stdErr) {
+    // console.log(stdErr)
+    if (error) {
+        console.log("\n" + stderr);
+    } else {
+        console.log(JSON.parse(stdOut));
+    }
+});
